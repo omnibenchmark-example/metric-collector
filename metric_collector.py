14c581bf253117c6c31d0146e856da5f6e28f0b9
Daniel Incicau <daniel.incicau@gmail.com>
2025-03-11 17:00:43 +0100

    Update metric collector

--- metric_collector.py
+++ metric_collector.py
@@ -5,13 +5,14 @@ def concatenate_input_content(input_files):
     concatenated_content = ""  # Initialize an empty string to hold the concatenated content
     
     # Iterate over each input file
-    for input_file in input_files:
-        # Open each input file in read mode and read its content
-        with open(input_file, 'r') as file:
-            # Read the content of the input file and append it to the concatenated_content string
-            concatenated_content += file.read()
-            # Optionally, you can add a newline between the content of each file
-            concatenated_content += '\n'
+    if input_files:
+        for input_file in input_files:
+            # Open each input file in read mode and read its content
+            with open(input_file, 'r') as file:
+                # Read the content of the input file and append it to the concatenated_content string
+                concatenated_content += file.read()
+                # Optionally, you can add a newline between the content of each file
+                concatenated_content += '\n'
 
     return concatenated_content
 
@@ -30,8 +31,9 @@ def aggregate_metrics_mapping(output_dir, input_files):
     with open(biometrics_report_html, 'w') as file:
         file.write("<html><body>\n")
         file.write("<h2>Input Files</h2>\n<ul>\n")
-        for file_name in input_files:
-            file.write(f"<li>{file_name}</li>\n")
+        if input_files:
+            for file_name in input_files:
+                file.write(f"<li>{file_name}</li>\n")
         
         file.write("</ul>\n</body></html>\n")
 
@@ -42,15 +44,15 @@ def main():
 
     # Add arguments
     parser.add_argument('--output_dir', type=str, help='output directory where method will store results.')
-    parser.add_argument('--metrics.mapping', type=str, nargs='+', help='Metrics mapping file.')
+    parser.add_argument('--metrics.scores', type=str, nargs='+', help='Metrics scores file.')
 
     # Parse arguments
     args, _ = parser.parse_known_args()
 
-    metrics_mapping_files = getattr(args, 'metrics.mapping')
+    metrics_scores_files = getattr(args, 'metrics.scores')
     output_dir = getattr(args, 'output_dir')
 
-    aggregate_metrics_mapping(output_dir, metrics_mapping_files)
+    aggregate_metrics_mapping(output_dir, metrics_scores_files)
 
 
 if __name__ == "__main__":
